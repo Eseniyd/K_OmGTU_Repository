58f723f533a73157d777a67e240e1b90171352f7
Eseniyd <145026658+Eseniyd@users.noreply.github.com>
2023-09-21 00:00:22 +0600

    Add files via upload

---
+++ Algorithmization and programming/pracrice work 20.09/образует ли заданная последовательность равномерно убывающую последовательность элементов.cs
@@ -0,0 +1,37 @@
+﻿class омгту2
+{
+    static void Main()
+    {
+        int kolichestvo, number, predid_number, raznica, perekluchatel = 1; // количество, число текущее, предыдущее число, разница, переключатель
+        kolichestvo = Convert.ToInt32(Console.ReadLine());
+        predid_number = Convert.ToInt32(Console.ReadLine());
+        number = Convert.ToInt32(Console.ReadLine());
+        raznica = predid_number - number;
+        if (raznica > 0)
+        {
+            for (int i = 0; i < kolichestvo - 2; i++)
+            {
+                predid_number = number;
+                number = Convert.ToInt32(Console.ReadLine());
+                if ((predid_number - number) != raznica)
+                {
+                    perekluchatel = 0;
+                }
+            }
+        }
+        else
+        {
+            perekluchatel = 0;
+        }
+        if (perekluchatel == 1)
+        {
+            Console.WriteLine("Последовательность является равномено убывающей");
+        }
+        else
+        {
+            Console.WriteLine("Последовательность не является равномено убывающей"); //образует ли заданная последовательность равномерно убывающую последовательность элементов
+        }
+    }
+}
+
+
