fbcec4c8083edbc6c0a2d06cdeeaabf0dbbf945c
Eseniyd <145026658+Eseniyd@users.noreply.github.com>
2023-09-20 23:15:12 +0600

    practice work 20.09

---
+++ Algorithmization and programming/pracrice work 20.09/все ли элементы кратны своему индексу.cs
@@ -0,0 +1,24 @@
+﻿class омгту2
+{
+    static void Main()
+    {
+        int n, k; // длина последовательности, число
+        int count = 0;
+        n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i <= n; i++)
+        {
+            k = Convert.ToInt32(Console.ReadLine());
+            if (k % i != 0)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count == 0); // все ли элементы последовательности кратны номеру под которым они считываются
+
+    }
+}
+
+
+
+
+
