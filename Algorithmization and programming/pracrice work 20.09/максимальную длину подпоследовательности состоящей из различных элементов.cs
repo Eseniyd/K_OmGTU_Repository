eda815ec7b5e65f6fd2340b7d8fe05d5d2026303
Eseniyd <145026658+Eseniyd@users.noreply.github.com>
2023-09-20 23:43:25 +0600

    Add files via upload

---
+++ Algorithmization and programming/pracrice work 20.09/максимальную длину подпоследовательности состоящей из различных элементов.cs
@@ -0,0 +1,33 @@
+﻿class омгту2
+{
+    static void Main()
+    {
+        int n, max_dlina, tek_posled = 1, number, predid_number;  //кол-во элементов, max длина, длина текущей послЕдовательности, текущее число, предыдущее число
+        n = Convert.ToInt32(Console.ReadLine());
+        max_dlina = 1;
+        predid_number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n - 1; i++)
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number != predid_number)
+            {
+                tek_posled++;
+
+            }
+            else
+            {
+                if ((max_dlina < tek_posled) & (tek_posled > 1))
+                {
+                    max_dlina = tek_posled;
+
+                }
+                tek_posled = 1;
+            }
+            predid_number = number;
+        }
+        Console.WriteLine(max_dlina); // максимальную длину подпоследовательности состоящей из различных элементов
+    }
+}
+
+
+
