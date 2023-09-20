1c99cfa932b6a4b754323fad4f92f1ad90ba6e6f
Eseniyd <145026658+Eseniyd@users.noreply.github.com>
2023-09-20 23:00:17 +0600

    минимальная длина подпоследовательности

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿class омгту2
+{
+    static void Main()
+    {
+        int n, min_dlina, tekDlina = 1, number, predid_number;  //кол-во элементов, минимальная длина, длина текущей послндовательности, текущее число, предыдущее число
+        n = Convert.ToInt32(Console.ReadLine());
+        min_dlina = n;
+        predid_number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n - 1; i++)
+        {
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number == predid_number)
+            {
+                tekDlina++;
+
+            }
+            else
+            {
+                if ((min_dlina > tekDlina) & (tekDlina > 1))
+                {
+                    min_dlina = tekDlina;
+
+                }
+                tekDlina = 1;
+            }
+            predid_number = number;
+        }
+        Console.WriteLine(min_dlina); // минимальная длина подпоследовательности
+    }
+}
+
+
+
+
+
