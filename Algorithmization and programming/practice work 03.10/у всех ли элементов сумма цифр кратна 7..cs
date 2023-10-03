fd63f4d507456294aea2e1c1a16d6c3661942882
Eseniyd <145026658+Eseniyd@users.noreply.github.com>
2023-10-03 22:48:07 +0600

    practice work 03.10

---
+++ Algorithmization and programming/practice work 03.10/у всех ли элементов сумма цифр кратна 7..cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography;
+using System.Threading.Channels;
+
+namespace омгту
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            bool proverka = true;
+            n = Convert.ToInt32(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int summ = 0;
+                while (a > 0)
+                {
+                    summ += a % 10;
+                    a /= 10;
+                }
+                if (summ % 7 != 0)
+                            proverka = false;
+            }
+            Console.WriteLine(proverka);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
